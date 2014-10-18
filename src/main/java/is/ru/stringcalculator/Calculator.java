20a57ae07a305bf687c6993ed8149e0e581c2811
Mr.Sigz <sigurgrimur13@ru.is>
2014-10-18 18:50:00 +0000

    test one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,7 +1,13 @@
 package is.ru.stringcalculator;
 
-public class Calculator{
-	public static int add(String test){
-	    	return 0;
+public class Calculator {
+
+	public static int add(String text){
+	    if(text.equals("")){
+		    return 0;
+		}
+		else{
+		    return 1;
+		}
 	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -7,9 +7,15 @@ public class CalculatorTest{
     public static void main(String args[]){
 	org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
-	@Test
-	 public void testEmptyString(){
-	    assertEquals(0, Calculator.add(""));
-	}
+    
+    @Test
+    public void testEmptyString(){
+	assertEquals(0, Calculator.add(""));
+    }
+
+    @Test
+    public void testOneNumber(){
+	assertEquals(1, Calculator.add("1"));
+   }
     
 }
