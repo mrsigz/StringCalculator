7d4ca821fba29fb634bd3e4ff4572fd6415d06c9
Mr.Sigz <sigurgrimur13@ru.is>
2014-10-18 20:50:38 +0000

    added test for multile numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -23,4 +23,8 @@ public class CalculatorTest{
 	assertEquals(3, Calculator.add("1,2"));
     }
     
+    @Test
+    public void testMultipleNumbers(){
+	assertEquals(6, Calculator.add("1,2,3"));
+    }
 }
